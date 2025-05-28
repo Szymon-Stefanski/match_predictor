387327b4117d3304aa6725a072f1f371ffcf57e9
Szymon-Stefanski <szymonstefanski1@gmail.com>
2025-05-28 22:50:48 +0200

    Split dataset into training and test sets

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("../data/ekstraklasa_2015_2025.csv", sep=",")
 
-print(df.columns)
+X = df.drop(columns=['Season', 'Result'])
+y = df['Result']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.2)
